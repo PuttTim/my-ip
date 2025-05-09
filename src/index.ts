0206cc1d8b2212ef342ab8cf99d44cab090a4885
PuttTim <yannaputt@gmail.com>
2025-05-10 01:29:51 +0800

    ✨ Added ASN/ASO

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 export default {
 	fetch(request, env, ctx): Response {
+		console.log(request.cf);
+
 		return new Response(
-			`${request.cf?.city} - ${request.cf?.country} ${
+			`${request.cf?.city} - ${request.cf?.country} - ${
 				request.headers.get('x-real-ip') ?? request.headers.get('cf-connecting-ip') ?? 'unknown'
-			}`
+			} - ${request.cf?.asOrganization}`
 		);
 	},
 } satisfies ExportedHandler<Env>;
