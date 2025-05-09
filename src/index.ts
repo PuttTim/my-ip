3fcfaae6d5393e9f64bbaf42bcb886e5928c17af
PuttTim <yannaputt@gmail.com>
2025-05-10 01:38:30 +0800

    remove console log

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 export default {
 	fetch(request, env, ctx): Response {
-		console.log(request.cf);
-
 		return new Response(
 			`${request.cf?.city} - ${request.cf?.country} - ${
 				request.headers.get('x-real-ip') ?? request.headers.get('cf-connecting-ip') ?? 'unknown'
